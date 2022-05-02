2d9113389918d6eff62019f0f2ce6dfbd5ed8205
mhihmi <mehdihilmi@gmail.com>
2022-05-02 16:20:00 +0200

    User Controller Login with mail encrypt

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -9,13 +9,12 @@ const db = require('../models');
 
 exports.signup = (req, res) => {
     const emailCryptoJs = cryptojs.HmacSHA256(req.body.email, process.env.MAIL_CRYPTO_KEY).toString();
-
     bcrypt.hash(req.body.password, 10) // hash request psw, salt hash number - method async
         .then(hash => {
             const user = new db.users({ // get hashed psw & save it in User Model instance for Database
+                ...req.body,
                 email: emailCryptoJs,
                 password: hash,
-                ...req.body
             });
             user.save() // save new user in Database
                 .then(() => res.status(201).json({ message: 'Utilisateur Créé !' }))
@@ -27,7 +26,7 @@ exports.signup = (req, res) => {
 exports.login = (req, res) => {
     const emailCryptoJs = cryptojs.HmacSHA256(req.body.email, process.env.MAIL_CRYPTO_KEY).toString();
 
-    User.findOne({ email: emailCryptoJs }) // Find User in DB with email sent in request, after encrypt
+    db.users.findOne({ where: { email: emailCryptoJs } }) // Find User in DB with email sent in request, after encrypt
         .then(user => {
             if (!user) {
                 return res.status(401).json({ error: 'Utilisateur introuvable !' }); // 401 => Unauthorized
@@ -39,8 +38,12 @@ exports.login = (req, res) => {
                     }
                     res.status(200).json({ // if true, Request Ok, object sent :
                         userId: user.id, // userId in DB
+                        isAdmin: user.isAdmin,
                         token: jwt.sign( // Encode token with jsw method sign()
-                            { userId: user.id }, // Check userId Request - Payload
+                            {
+                                userId: user.id,
+                                isAdmin: user.isAdmin
+                            }, // Check userId Request & if Admin - Payload
                             process.env.JWT_KEY, // Encode secret key - Salt
                             { expiresIn: '24h' }
                         )
