ec03d7cb35acb1240c3678a93192914e69f5b87b
mhihmi <mehdihilmi@gmail.com>
2022-05-23 21:32:40 +0200

    Add Toggle switch button

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,6 +1,14 @@
 <template>
   <footer class="home-footer">
-    <img alt="Grouporama logo" src="../assets/logo.svg" />
+    <label class="switch">
+      <input type="checkbox" checked />
+      <span class="slider round"></span>
+    </label>
+
+    <!-- <input type="checkbox" id="switch" />
+    <label for="switch" class="switch-label">
+      <div class="toggle"></div>
+    </label> -->
   </footer>
 </template>
 
@@ -12,4 +20,5 @@ export default {
 
 <style scoped lang="scss">
 @import "@/styles/layout/footer";
+@import "@/styles/components/switch";
 </style>
