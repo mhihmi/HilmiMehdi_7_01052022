afc6f3a041ccd759ce7d70c7555a612ef8f32914
mhihmi <mehdihilmi@gmail.com>
2022-10-07 17:41:38 +0200

    Like controller Fix

--- backend/controllers/like.js
+++ backend/controllers/like.js
@@ -1,17 +1,23 @@
 const db = require('../models');
 
 exports.addLike = (req, res) => {
+    db.likes.findOne({
+        where: { postId: req.params.id, userId: req.token.userId }
+    }).then((like) => {
+        if (like == null) {
+            const newLike = {
+                userId: req.token.userId, // get token userId  
+                like: true,
+                postId: req.params.id
+            }
 
-    const like = {
-        userId: req.token.userId, // get token userId  
-        like: true,
-        postId: req.params.id
-    }
-
-    db.likes.create(like)
-        .then(() => res.status(201).json({ message: 'Post Liked successfully!' }))
-        .catch(error => res.status(400).json({ error }));
-
+            db.likes.create(newLike)
+                .then(() => res.status(201).json({ message: 'Post Liked successfully!' }))
+                .catch(error => res.status(400).json({ error }));
+        } else {
+            res.status(401).json({ Message: 'Unauthorized Request !' });
+        }
+    }).catch(error => res.status(400).json({ error }));
 };
 exports.getAllLikes = (req, res) => {
     db.likes.findAll()
