f8a23192850a99e5682e3410c24e689beaf6e028
mhihmi <mehdihilmi@gmail.com>
2023-01-05 17:55:11 +0100

    Pkg update

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,7 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import { createPinia } from "pinia";
+import { createPinia } from 'pinia'
 
 // window.vm = createApp(App).use(createPinia()).use(router).mount("#app");
-createApp(App).use(createPinia()).use(router).mount("#app");
+createApp(App).use(createPinia()).use(createPinia()).use(router).mount("#app");
