d856d9c61d36aa2fa8f5498a91e3d95d023503b7
mhihmi <mehdihilmi@gmail.com>
2024-04-28 13:57:38 +0200

    Fix update app for node 20

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,7 +4,6 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import { createPinia } from "pinia";
-import { createPinia } from 'pinia'
 
 // window.vm = createApp(App).use(createPinia()).use(router).mount("#app");
 createApp(App).use(createPinia()).use(createPinia()).use(router).mount("#app");
